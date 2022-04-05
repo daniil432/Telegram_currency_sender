64cb8e4a0dbc76584a3af3e81bf60988706af8c7
daniil432 <lfybbk1399123@gmail.com>
2022-04-05 18:55:25 +0300

    Пришлось убрать запуск бота из TeleBot\apps.py При запуске Celery инициализируется так же, как и сам Django-сервер, что в принципе логично, так что бот запускается снова и не даёт питону добраться до периодических задач. Так что запуск бота будет осуществляться через консоль. Подробности напишу в описании проекта на github и в README.txt

--- TeleBot/apps.py
+++ TeleBot/apps.py
@@ -1,12 +1,7 @@
 from django.apps import AppConfig
-import TeleBot.telebot_commands
 
 
 class TelebotConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'TeleBot'
 
-    def ready(self):
-        # Запуск бота. Пока бот работает, Django сервер до конца не поднимется. Но, тем не менее, все необходимые для
-        # бота модули уже загрузились. Если когда-нибудь будут возникать ошибки, найду другое решение.
-        TeleBot.telebot_commands.TeleBot()
